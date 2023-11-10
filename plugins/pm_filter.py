bb6adbf68ae3d3106263b36331570c048936401a
Ayush Khanduri <113684157+ayushkhanduri74@users.noreply.github.com>
2023-11-10 16:54:05 +0000

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -111,51 +111,21 @@ async def next_page(bot, query):
         ENABLE_SHORTLINK = False
     if ENABLE_SHORTLINK == True:
         if settings['button']:
-            btn = [
-                [
-                    InlineKeyboardButton(
-                        text=f"❄{get_size(file.file_size)}✨{file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
-                    ),
-                ]
-                for file in files
-            ]
+            btn = [[
+                    InlineKeyboardButton(text=f"❄[{get_size(file.file_size)}]✨{file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files]
         else:
-            btn = [
-                
-                [
-                    InlineKeyboardButton(
-                        text=f"{file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
-                    ),
-                    InlineKeyboardButton(
-                        text=f"{get_size(file.file_size)}",
-                        url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
-                    ),
-                ]
-                for file in files
-            ]
+            btn = [[
+                    InlineKeyboardButton(text=f"{file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")),
+                    InlineKeyboardButton(text=f"{get_size(file.file_size)}",url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files]
     else:
         if settings['button']:
-            btn = [
-                [
-                    InlineKeyboardButton(
-                        text=f"❄{get_size(file.file_size)}✨{file.file_name}", callback_data=f'files#{file.file_id}'
-                    ),
-                ]
-                for file in files
-            ]
+            btn = [[
+                    InlineKeyboardButton(text=f"❄[{get_size(file.file_size)}]✨{file.file_name}", callback_data=f'files#{file.file_id}')] for file in files ]
         else:
-            btn = [
-                [
-                    InlineKeyboardButton(
-                        text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
-                    ),
-                    InlineKeyboardButton(
-                        text=f"{get_size(file.file_size)}",
-                        callback_data=f'files_#{file.file_id}',
-                    ),
-                ]
-                for file in files
-        ]
+            btn = [[
+                    InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'files#{file.file_id}'),
+                    InlineKeyboardButton(text=f"{get_size(file.file_size)}",callback_data=f'files_#{file.file_id}',)] for file in files 
+            ]
     btn.insert(0, 
         [
             InlineKeyboardButton(f' 🎬 {search} 🎬 ', 'qinfo')
@@ -1291,52 +1261,20 @@ async def auto_filter(client, msg, spoll=False):
     pre = 'filep' if settings['file_secure'] else 'file'
     if ENABLE_SHORTLINK == True:
         if settings["button"]:
-            btn = [
-                [
-                    InlineKeyboardButton(
-                        text=f"❄{get_size(file.file_size)}✨{file.file_name}", url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
-                    ),
-                ]
-                for file in files
-            ]
+            btn = [[
+                    InlineKeyboardButton(text=f"❄[{get_size(file.file_size)}]✨{file.file_name}", url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}"))] for file in files ]
         else:
-            btn = [
-                [
-                    InlineKeyboardButton(
-                        text=f"{file.file_name}",
-                        url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
-                    ),
-                    InlineKeyboardButton(
-                        text=f"{get_size(file.file_size)}",
-                        url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
-                    ),
-                ]
-                for file in files
-            ]
+            btn = [[
+                    InlineKeyboardButton(text=f"{file.file_name}", url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")),
+                    InlineKeyboardButton(text=f"{get_size(file.file_size)}",url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")),] for file in files ]
     else:
         if settings["button"]:
-            btn = [
-                [
-                    InlineKeyboardButton(
-                        text=f"❄{get_size(file.file_size)}✨{file.file_name}", callback_data=f'{pre}#{file.file_id}'
-                    ),
-                ]
-                for file in files
-            ]
+            btn = [[
+                    InlineKeyboardButton(text=f"❄[{get_size(file.file_size)}]✨{file.file_name}", callback_data=f'{pre}#{file.file_id}')] for file in files ]
         else:
-            btn = [
-                [
-                    InlineKeyboardButton(
-                        text=f"{file.file_name}",
-                        callback_data=f'{pre}#{file.file_id}',
-                    ),
-                    InlineKeyboardButton(
-                        text=f"{get_size(file.file_size)}",
-                        callback_data=f'{pre}#{file.file_id}',
-                    ),
-                ]
-                for file in files
-            ]
+            btn = [[
+                    InlineKeyboardButton(text=f"{file.file_name}",callback_data=f'{pre}#{file.file_id}',),
+                    InlineKeyboardButton(text=f"{get_size(file.file_size)}",callback_data=f'{pre}#{file.file_id}',)] for file in files ]
     btn.insert(0, 
         [
             InlineKeyboardButton(f' 🎬 {search} 🎬 ', 'qinfo')
